fd374a6755484db7babf0eecff83db4a697f29c4
Adam Fahrer <fahreradam@gmail.com>
2024-03-22 22:49:55 -0400

    Changed file names and moved files around

--- CleanupClash/Source/CleanupClash/CleanupClashGameMode.cpp
+++ CleanupClash/Source/CleanupClash/CleanupClashGameMode.cpp
@@ -1,13 +1,13 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "CleanupClashGameMode.h"
-#include "CleanupClashCharacter.h"
+#include "PlayerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ACleanupClashGameMode::ACleanupClashGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/BP_Player.BP_Player"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
