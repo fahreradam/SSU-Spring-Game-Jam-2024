5fadbceb92511a273696ca409ecb3e2ffada3214
Adam Fahrer <fahreradam@gmail.com>
2024-03-22 22:03:30 -0400

    Added base project... again

---
+++ CleanupClash/Source/CleanupClash/CleanupClashGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CleanupClashGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACleanupClashGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACleanupClashGameMode();
+};
+
+
+
